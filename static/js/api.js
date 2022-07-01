561084a7e9e23d221f65411c578593b202750fd5
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-01 23:46:55 +0900

    Feat : logout 기능 구현

--- static/js/api.js
+++ static/js/api.js
@@ -1,3 +1,6 @@
+const backend_base_url = 'http://127.0.0.1:8000'
+const frontend_base_url = 'http://127.0.0.1:5500/templates'
+
 // 회원가입 //
 async function handleSignup() {
     const signupData = {
@@ -7,7 +10,7 @@ async function handleSignup() {
         fullname: document.getElementById("floatingInputFullname").value,
     }
 
-    const response = await fetch(`http://127.0.0.1:8000/user/`, {
+    const response = await fetch(`${backend_base_url}/user/`, {
         headers: {
             Accept: "application/json",
             'Content-type': 'application/json'
@@ -20,7 +23,7 @@ async function handleSignup() {
 
     if (response.status == 200) {
         console.log("여기", response_json)
-        window.location.replace(`http://127.0.0.1:5500/templates/login.html`)
+        window.location.replace(`${frontend_base_url}/login.html`)
     } else {
         console.log("여기11", response_json)
         alert(response.status)
@@ -34,7 +37,7 @@ async function handleLogin() {
         password: document.getElementById("floatingPassword").value,
     }
 
-    const response = await fetch(`http://127.0.0.1:8000/user/api/token/`, {
+    const response = await fetch(`${backend_base_url}/user/api/season/token/`, {
         headers: {
             Accept: "application/json",
             'Content-type': 'application/json'
@@ -57,9 +60,37 @@ async function handleLogin() {
         }).join(''));
 
         localStorage.setItem("payload", jsonPayload);
-        window.location.replace(`http://127.0.0.1:5500/templates/index.html`)
+        window.location.replace(`${frontend_base_url}/index.html`)
     } else {
         alert(response.status)
     }
-
 }
+
+// 로그아웃 //
+async function logout() {
+    localStorage.removeItem('payload')
+    localStorage.removeItem('access')
+    localStorage.removeItem('refresh')
+
+    window.location.replace(`${frontend_base_url}/login.html`)
+}
+
+
+// async function getUser() {
+//     const response = await fetch(`${backend_base_url}/user/`, {
+//         headers: {
+//             'Authorization': localStorage.getItem('access')
+//         }
+//     })
+//     console.log(response)
+
+//     if (response.status == 200) {
+//         response_json = await response.json()
+//         console.log(response_json)
+//         return response_json
+//     } else {
+//         return null
+//     }
+// }
+
+// getUser()
