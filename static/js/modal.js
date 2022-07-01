1a24a74627a06c95ff54c0b19970e9c5bd3bb88c
juhyun1011 <juhyun4567@gmail.com>
2022-07-01 15:56:56 +0900

    Feat : 게시글 업로드 기능

--- static/js/modal.js
+++ static/js/modal.js
@@ -12,3 +12,30 @@ $(function () {
         $("#popup").fadeOut(); //페이드아웃
     }
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal_open");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
