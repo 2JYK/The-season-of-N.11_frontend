4c0fae3c3d2ca20ff980d2a8890ecae9b38c0068
GyeongSuKim <rla04722@naver.com>
2022-07-03 20:59:41 +0900

    Feat : bookmark, like API
    bookmark, like POST방식 API 적용

--- static/js/api.js
+++ static/js/api.js
@@ -2,6 +2,7 @@
 const backend_base_url = "http://127.0.0.1:8000/"
 const frontend_base_url = "http://127.0.0.1:5500/"
 
+
 function open_modal(id) {
   $("#popup" + id).css('display', 'flex').hide().fadeIn();
   //팝업을 flex속성으로 바꿔준 후 hide()로 숨기고 다시 fadeIn()으로 효과
@@ -9,12 +10,11 @@ function open_modal(id) {
 
 function close_modal(id) {
   // id 파라미터가 str값 으로 넘어와서 slice하고 int로 변환
-  a = parseInt(id.slice(1)) // 변수명 바꿔야 함
+  parse_int = parseInt(id.slice(1)) // 변수명 바꿔야 함
   function modal_close() {
-    $("#popup" + a).fadeOut(); //페이드아웃
+    $("#popup" + parse_int).fadeOut(); //페이드아웃
   }
-
-  $("#close" + a)
+  $("#close" + parse_int)
   modal_close(); //모달 닫기);
 }
 
@@ -33,10 +33,10 @@ function show_article() {
           postings[i].title,
           postings[i].content,
           postings[i].comments
-        )
-      }
-      function append_temp_html(id, user, title, content, comments, img) {
-        temp_html = `
+          )
+        }
+        function append_temp_html(id, user, title, content, comments, img) {
+          temp_html = `
           <li>
           <div class="card" style="width: 18rem;" id="${id}" onClick="open_modal(this.id)">
           <div class="card-img" style="background: rgb(192, 236, 155);">
@@ -48,106 +48,103 @@ function show_article() {
           <p class="card-text">
           ${content}
           </p>
-          <div class="icons">
-          <i class="far fa-heart" style="font-size:24px"></i>
-          <i class="fa fa-bookmark-o" style="font-size:24px"></i>
           </div>
-              </div>
-              </div>
-              
-              <!-- 게시글 상세페이지 모달 -->
-              <div class="popup-wrap" id="popup${id}">
-              <div class="popup">
-              
-              <!-- 게시글 상세페이지 모달창 헤더 -->
-              <div class="popup-header">
-              <span></span>
-              <h2>${user} 님의 게시물</h2>
-              <span></span>
-              <span id="1${id}" class="popup-close" onClick="close_modal(this.id)"> X </span>
-              </div>
-              
-              <!-- 게시글 상세페이지 모달창 바디 -->
-              <div class="popup-body">
-              <div class="popup-img" style="background: rgb(141, 206, 214);">
-              <!--이미지 삽입 예정-->
-              </div>
-              <h2 class="popup-title">
-              ${title}
-              </h2>
-              <hr>
-              <h5 class="popup-content">
-              ${content}
-              </h5>
-              <hr>
-              </div>
-              <!-- 게시글 상세페이지 모달창 댓글 output -->
-              <div class="popup-comment" id="comment${id}">
-              <h1>댓글 창</h1>
-                    <hr>
+          </div>
 
-                      </div>
-                      
-                      <!-- 게시글 상세페이지 모달창 댓글 input -->
-                      <div class="popup-post-comment">
-                      <input class="popup-post-input" id="comment_input${id}" type="text" placeholder="댓글을 입력 해주세요..." />
-                  <button class="popup-post-input-btn" onclick="post_comment(${id})">
-                    저장
-                    </button>
-                    </div>
-                    </div>
-                    </div>
-                    </li> 
-                    `
-        $('#card').append(temp_html)
-        for (let j = 0; j < comments.length; j++) {
-          $(`#comment${id}`).append(`<p>${comments[j].username} : ${comments[j].content}</p>
-                      <hr>`)
+          <div class="icons">
+          <i class="far fa-heart" style="font-size:24px" onclick="post_like(${id})"></i>
+          <i class="fa fa-bookmark-o" style="font-size:24px" onclick="post_bookmark(${id})"></i>
+          </div>
+          
+          <!-- 게시글 상세페이지 모달 -->
+          <div class="popup-wrap" id="popup${id}">
+          <div class="popup">
+          
+          <!-- 게시글 상세페이지 모달창 헤더 -->
+          <div class="popup-header">
+          <span></span>
+          <h2>${user} 님의 게시물</h2>
+          <span></span>
+          <span id="1${id}" class="popup-close" onClick="close_modal(this.id)"> X </span>
+          </div>
+          
+          <!-- 게시글 상세페이지 모달창 바디 -->
+          <div class="popup-body">
+          <div class="popup-img" style="background: rgb(141, 206, 214);">
+          <!--이미지 삽입 예정-->
+          </div>
+          <h2 class="popup-title">
+          ${title}
+          </h2>
+          <hr>
+          <h5 class="popup-content">
+          ${content}
+          </h5>
+          <hr>
+          </div>
+          <!-- 게시글 상세페이지 모달창 댓글 output -->
+          <div class="popup-comment" id="comment${id}">
+          <h1>댓글 창</h1>
+          <hr>
+          
+          </div>
+          
+          <!-- 게시글 상세페이지 모달창 댓글 input -->
+          <div class="popup-post-comment">
+          
+          <input class="popup-post-input" id="comment_input${id}" type="text" placeholder="댓글을 입력 해주세요..." />
+          <button class="popup-post-input-btn" onclick="post_comment(${id})">
+          저장
+          </button>
+          </div>
+          </div>
+          </div>
+          </li> 
+          `
+          $('#card').append(temp_html)
+          
+          for (let j = 0; j < comments.length; j++) {
+            $(`#comment${id}`).append(`<p>${comments[j].username} : ${comments[j].content}</p>
+            <hr>`)
+          }
         }
       }
+    });
+  } show_article()
+  
+  
+  // 게시글 작성
+  async function post_article() {
+    const title = document.getElementById("title").value
+    const content = document.getElementById("content").value
+    
+    const articleData = {
+      title: title,
+      content: content,
     }
-  });
-} show_article()
-
-
-// 게시글 작성
-async function post_article() {
-  const title = document.getElementById("title").value
-  const content = document.getElementById("content").value
-
-  const articleData = {
-    title: title,
-    content: content,
+    
+    const response = await fetch(`${backend_base_url}article/`, {
+      method: 'POST',
+      
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer " + localStorage.getItem("access")
+      },
+      body: JSON.stringify(articleData)
+    }
+    )
+    
+    response_json = await response.json()
+    console.log(response_json)
+    
+    if (response.status == 200) {
+      window.location.replace(`${frontend_base_url}/`);
+    } else {
+      alert(response.status)
+    }
   }
-
-  const response = await fetch(`${backend_base_url}article/`, {
-    method: 'POST',
-
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': "Bearer " + localStorage.getItem("access")
-    },
-    body: JSON.stringify(articleData)
-  }
-  )
-
-  response_json = await response.json()
-  console.log(response_json)
-
-  if (response.status == 200) {
-    window.location.replace(`${frontend_base_url}/`);
-  } else {
-    alert(response.status)
-  }
-}
-
-// function refresh(id) {
-//   console.log("여기2")
-//   const a = "#popup" + id
-//   console.log(a)
-//   $("a").load(location.href + " a");
-//   console.log($(a).load(location.href + a))
-// }
+    
+    
 //댓글 작성
 async function post_comment(id) {
   const content = document.getElementById("comment_input" + id).value
@@ -155,7 +152,7 @@ async function post_comment(id) {
     "article": id,
     "content": content
   }
-
+      
   const response = await fetch(`${backend_base_url}article/comment/`, {
     method: 'POST',
     headers: {
@@ -165,17 +162,71 @@ async function post_comment(id) {
     body: JSON.stringify(commentData)
   }
   )
-
+  
   if (response.status == 200) {
     // refresh(id)
-    window.location.reload()
+    window.location.reload();
+
     return response
+
   } else {
     alert(response.status)
   }
 }
 
 
+// 북마크
+async function post_bookmark(id) {
+  const bookmarkData = {
+    "article": id,
+  }
+  const response = await fetch(`${backend_base_url}article/bookmark/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer " + localStorage.getItem("access")
+    },
+    body: JSON.stringify(bookmarkData)
+  }
+  )
+  response_json = await response.json()
+  console.log('북마크', response_json)
+  
+  if (response.status == 200) {
+    alert("북마크가 되었습니다")
+    return response
+
+  } else {
+    alert("북마크가 취소 되었습니다")
+  }
+}
+
+
+// 좋아요
+async function post_like(id) {
+  const likeData = {
+    "article": id,
+  }
+  const response = await fetch(`${backend_base_url}article/like/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer " + localStorage.getItem("access")
+    },
+    body: JSON.stringify(likeData)
+  }
+  )
+  response_json = await response.json()
+  console.log('좋아요 :', response_json)
+  
+  if (response.status == 200) {
+    alert("좋아요를 하셨습니다")
+    return response
+
+  } else {
+    alert("좋아요를 취소 하셨습니다.")
+  }
+}
 
 
 // 회원가입 //
@@ -186,7 +237,7 @@ async function handleSignup() {
     email: document.getElementById("floatingInputEmail").value,
     fullname: document.getElementById("floatingInputFullname").value,
   }
-
+  
   const response = await fetch(`http://127.0.0.1:8000/user/`, {
     headers: {
       Accept: "application/json",
@@ -252,5 +303,5 @@ async function logout() {
   localStorage.removeItem('access')
   localStorage.removeItem('refresh')
 
-  window.location.replace(`${frontend_base_url}/templates/login.html`)
+  window.location.replace(`${frontend_base_url}templates/login.html`)
 }
